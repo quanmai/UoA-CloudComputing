79a31510b31d3896d8315af0897f89f8146fc6f6
Quan Mai <40239882+quan-maithanh@users.noreply.github.com>
2021-09-22 22:58:27 -0500

    Create BigramCount.java

---
+++ BigramCount/BigramCount.java
@@ -0,0 +1,72 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+public class BigramCount {
+  public static class BigramMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+    private final static IntWritable one = new IntWritable(1);
+    private final static Text bigram = new Text();
+    String word = null;
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        String next = itr.nextToken();
+
+        if (word != null) {
+                bigram.set(word + " " + next);
+                context.write(bigram, one);
+        }
+        word = next;
+      }
+    }
+
+    protected void cleanup(Context context) throws IOException,
+                        InterruptedException {
+                Text t1=new Text("zzzzzzzzz Times Map task lanched: ");
+                context.write(t1,one);
+    }
+  }
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: wordcount <in> <out>");
+      System.exit(2);
+    }
+    Job job = new Job(conf, "word count");
+    job.setJarByClass(BigramCount.class);
+    job.setMapperClass(BigramMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    //MultipleOutputs.addNamedOutput(job, JobOutputName, TextOutputFormat.class, Text.class, IntWritable.class);
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
